9462bdba909c86fd3dc0cf4622f91dd3a81d499c
Cheng Hui-Chin <hccheng091@gmail.com>
2026-09-09 15:58:57 +0800

    first commit

---
+++ src/test_setup.py
@@ -0,0 +1,10 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import mne
+
+print("PrecisionSleep environment ready!")
+
+print("NumPy:", np.__version__)
+print("Pandas:", pd.__version__)
+print("MNE:", mne.__version__)
